f46ff75cefb3e8900c5dc4256dd3eaf70a10edca
Monnnoha <chinthapallimohan19@gmail.com>
2026-03-10 02:06:52 +0530

    init commit

---
+++ foodredistribution/src/main/java/com/foodredistribution/foodredistribution/controller/DonationController.java
@@ -0,0 +1,20 @@
+package com.foodredistribution.foodredistribution.controller;
+
+import com.foodredistribution.foodredistribution.entity.FoodDonation;
+import com.foodredistribution.foodredistribution.service.FoodDonationService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+
+@RestController
+@RequestMapping("/api/donations")
+public class DonationController {
+
+    @Autowired
+    private FoodDonationService donationService;
+
+    @PostMapping
+    public FoodDonation createDonation(@RequestBody FoodDonation donation) {
+        return donationService.createDonation(donation);
+    }
+}
